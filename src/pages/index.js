e70a8df64f156a0a941f7950c659f36bedf6099d
Isabel Blomström <isabel.blomstrom@gmail.com>
2020-09-25 16:03:14 +0200

    Added titles and prices with mocked data

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,45 @@ import imageChairs from "../images/pastel-chairs.jpg"
 import imageBags from "../images/pastel-bags.jpg"
 import imageHeadphones from "../images/pastel-headphones.jpg"
 
-const IndexPage = () => (
+const ProductData = [
+  {
+    title: "Car ride",
+    price: 550,
+    id: 1
+  },
+  {
+    title: "Concert",
+    price: 450,
+    id: 2
+  },
+  {
+    title: "Bags",
+    price: 250,
+    id: 3
+  },
+  {
+    title: "Headphones",
+    price: 300,
+    id: 4
+  }
+  ]
+
+
+const IndexPage = () => {
+  let car = ProductData.find(
+    product => product.id === 1);
+
+    let concert = ProductData.find(
+      product => product.id === 2);
   
-  <Layout>
+  let bags = ProductData.find(
+    product => product.id === 3);
+    
+    let headphones = ProductData.find(
+      product => product.id === 4);
+
+  return (
+   <Layout>
     <SEO title="Home" />
     <h1>Welcome!</h1>
     <hr className="hr"/>
@@ -20,14 +56,41 @@ const IndexPage = () => (
       quae tenetur reprehenderit. Dolores quibusdam similique optio!</p>
 
     <div className="imgDiv">
-      <img src={imageCar} width="300" height="auto" alt="car" />
-      <img src={imageChairs} width="300" height="auto" alt="chairs" />
-      </div>
+    <div>
+        <img src={imageCar} width="300" height="auto" alt="bags" />
+        <div style={{display: 'flex', justifyContent: 'space-around'}}>
+          <p>{car.title}</p>
+          <p>{car.price}:-</p>
+          </div>
+        </div>
+      
+      <div>
+        <img src={imageChairs} width="300" height="auto" alt="bags" />
+        <div style={{display: 'flex', justifyContent: 'space-around'}}>
+          <p>{concert.title}</p>
+          <p>{concert.price}:-</p>
+          </div>
+        </div>
+        </div>
+
       <div className="imgDiv2">
-      <img src={imageBags} width="300" height="auto" alt="bags" />
-      <img src={imageHeadphones} width="300" height="auto" alt="headphones" />
+        <div>
+        <img src={imageBags} width="300" height="auto" alt="bags" />
+        <div style={{display: 'flex', justifyContent: 'space-around'}}>
+          <p>{bags.title}</p>
+          <p>{bags.price}:-</p>
+          </div>
+        </div>
+      
+        <div>
+        <img src={imageHeadphones} width="300" height="auto" alt="bags" />
+        <div style={{display: 'flex', justifyContent: 'space-around'}}>
+          <p>{headphones.title}</p>
+          <p>{headphones.price}:-</p>
+          </div>
+        </div>
     </div>
   </Layout>
-)
+)}
 
 export default IndexPage
